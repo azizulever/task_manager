ae07e6771ae5e99667cd9c53d0fd7dd65d2909dd
Azizul Islam <contact.azizulislam@gmail.com>
2024-10-29 09:37:30 +0600

    Sign in page created

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:task_manager/ui/screens/splash_screen.dart';
 
-class TaskManagerApp extends StatelessWidget {
+class TaskManagerApp extends StatefulWidget {
   const TaskManagerApp({super.key});
 
   @override
+  State<TaskManagerApp> createState() => _TaskManagerAppState();
+}
+
+class _TaskManagerAppState extends State<TaskManagerApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(),
+        inputDecorationTheme: _inputDecorationTheme(),
+        elevatedButtonTheme: _el
+      ),
+      home: const SplashScreen(),
+    );
+  }
+
+  InputDecorationTheme _inputDecorationTheme() {
+    return InputDecorationTheme(
+      fillColor: Colors.white,
+      filled: true,
+      border: _inputBorder(),
+      enabledBorder: _inputBorder(),
+      errorBorder: _inputBorder(),
+      focusedBorder: _inputBorder(),
+    );
+  }
+
+  OutlineInputBorder _inputBorder() {
+    return OutlineInputBorder(
+      borderSide: BorderSide.none,
+      borderRadius: BorderRadius.circular(8),
     );
   }
 }
--- lib/ui/screens/sign_in_screen.dart
+++ lib/ui/screens/sign_in_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/utils/app_colors.dart';
+import 'package:task_manager/ui/widgets/screen_background.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -10,6 +12,36 @@ class SignInScreen extends StatefulWidget {
 class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
+      body: ScreenBackground(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 90),
+              Text('Get Started With',
+                  style: textTheme.titleLarge
+                      ?.copyWith(fontSize: 28, fontWeight: FontWeight.w600)),
+              const SizedBox(height: 20),
+              TextFormField(
+                decoration: const InputDecoration(hintText: 'Email'),
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                decoration: const InputDecoration(hintText: 'Password'),
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Icon(Icons.arrow_forward_ios),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
